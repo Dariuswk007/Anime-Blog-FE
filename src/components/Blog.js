d5824964a0cf12794d62ab80595b8350233c18e3
Darius007 <dariuswk@aol.com>
2022-09-21 01:38:04 -0500

    added styles and working on blog user render

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 export default function Blog() {
+
+    const [allUsers, setAllUsers] = useState([]);
+
+    useEffect(() => {
+        fetch("https://hidden-mountain-30566.herokuapp.com/user/get")
+        .then((res) => res.json())
+        .then((res) => setAllUsers(res))
+        .catch((error) => console.log("there was an error retrieving your data.", error));
+      }, []);
+
+    const renderAllUsers = () => {
+        return allUsers.map((user) => {
+            return (
+                {user,username, password}
+            )
+        })
+    }
+
   return (
-    <div>This will be for the blogs page</div>
+    <div>
+        BLOGS
+    </div>
   )
 }
